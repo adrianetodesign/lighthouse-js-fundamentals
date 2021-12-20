32d69d67576cd3dfa692962d63bb010a30f4da8b
adrianetodesign <adrianetodesign@gmail.com>
2021-12-20 23:42:57 +0000

    Completed file for the Loopy Lighthouse Assignment

---
+++ loopy-lighthouse.js
@@ -0,0 +1,20 @@
+/* 
+Loopy Lighthouse
+Adrian To
+20/12/2021
+*/
+
+for (let i = 100; i <= 200; i++) {
+  if (i % 3 == 0 && i % 4 == 0) {
+    console.log("LoopyLighthouse");
+  }
+  else if (i % 3 == 0) {
+    console.log("Loopy");
+  }
+  else if (i % 4 == 0) {
+    console.log("Lighthouse");
+  } 
+  else {
+    console.log(i);
+  }
+}

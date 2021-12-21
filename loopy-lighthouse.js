d03a4c58108a6692c340946f32d49c6596626fe1
adrianetodesign <adrianetodesign@gmail.com>
2021-12-21 07:36:38 +0000

    minor corrections to hell.js and loopy-lighthouse.js

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -5,13 +5,13 @@ Adrian To
 */
 
 for (let i = 100; i <= 200; i++) {
-  if (i % 3 == 0 && i % 4 == 0) {
+  if (i % 3 === 0 && i % 4 === 0) {
     console.log("LoopyLighthouse");
   }
-  else if (i % 3 == 0) {
+  else if (i % 3 === 0) {
     console.log("Loopy");
   }
-  else if (i % 4 == 0) {
+  else if (i % 4 === 0) {
     console.log("Lighthouse");
   } 
   else {

135a305c3152f12fa133bbdf15055d5b2af3bdad
adrianetodesign <adrianetodesign@gmail.com>
2021-12-21 17:58:07 +0000

    ice-cream.js created and assignment completed.

---
+++ ice-cream.js
@@ -0,0 +1,16 @@
+/*
+Ice Cream
+Adrian To
+20/12/2021
+*/
+
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);

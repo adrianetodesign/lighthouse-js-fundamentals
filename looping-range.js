3669849dcb4d2ff7b0be195f6004632655989640
adrianetodesign <adrianetodesign@gmail.com>
2021-12-22 23:13:56 +0000

    looping-range.js rewritten to return empty array. Failed to properly read instructions, need more caffeine.

--- looping-range.js
+++ looping-range.js
@@ -6,20 +6,24 @@ Adrian To
 
 const range = function(start, end, step) {
   let finalArray = [];
-  if (start == undefined || end == undefined || step == undefined) {
-    return undefined;
+  if (start === undefined || end === undefined || step === undefined) {
+    return finalArray;
   } else if (start > end) {
-    return undefined;
+    return finalArray;
   } else if (step <= 0) {
-    return undefined;
+    return finalArray;
   } else {
     for (let i = start; i <= end; i += step) {
       finalArray.push(i);
     }
+    return finalArray;
   }
-  return finalArray;
 }
 
 console.log(range(0, 10, 2));
 console.log(range(10, 30, 5));
 console.log(range(-5, 2, 3));
+console.log(range());
+console.log(range(2,1,1));
+console.log(range(1,5,0));
+console.log(range(1.5,-1));

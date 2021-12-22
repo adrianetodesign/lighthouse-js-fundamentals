2743f01fd630a2b4bf2261a81e8a3290d3d7eac3
adrianetodesign <adrianetodesign@gmail.com>
2021-12-22 23:26:34 +0000

    created concat-arrays.js for assignment.

---
+++ concat-arrays.js
@@ -0,0 +1,21 @@
+/*
+Concatenate Arrays
+Adrian To
+22/12/2021
+
+We've sent out two researchers to determine, on a scale of 1-10, 
+how much people like lighthouses and they are hitting the streets 
+of Canada to find out what people think. Both researchers have 
+stored their numbers in array for each location. This means that 
+we have two different arrays of data for every single location, 
+but we only want a single array for each location. We're going to 
+have to concatenate the arrays from both researchers into a single array.
+
+Define a function called concat
+
+The function should, when given two arrays, concatenate the arrays together.
+*/
+
+const concat = function() {
+  
+}

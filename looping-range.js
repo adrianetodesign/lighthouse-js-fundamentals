d0cf38e3f5dc2a400d0db340463297328b80ca25
adrianetodesign <adrianetodesign@gmail.com>
2021-12-22 22:57:39 +0000

    initial design for Looping Range assignment.

---
+++ looping-range.js
@@ -0,0 +1,25 @@
+/*
+Looping Range
+Adrian To
+22/12/2021
+*/
+
+const range = function(start, end, step) {
+  let finalArray = [];
+  if (start === undefined || end === undefined || step === undefined) {
+    return undefined;
+  } else if (start > end) {
+    return undefined;
+  } else if (step <= 0) {
+    return undefined;
+  } else {
+    for (let i = start; i <= end; i + step) {
+      finalArray.push(i);
+    }
+  }
+  return finalArray;
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
